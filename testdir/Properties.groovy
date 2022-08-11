3a69472ec85130d39c7a36ab404e7349c630bdee
Yasassri Ratnayake <ycrnet@gmail.com>
2022-08-10 20:33:33 -0400

    Update Properties.groovy

--- testdir/Properties.groovy
+++ testdir/Properties.groovy
@@ -1,44 +1,44 @@
 //Class Properties {
-//   def WEB_SERVER
-//   def DB_SERVER
-//   def DB_NAME
-//   def SITE_URL
+  def WEB_SERVER
+  def DB_SERVER
+  def DB_NAME
+  def SITE_URL
   
-//   def setEnvironments(ENV) {
-//      switch (ENV) {
-//       case 'site_01':
-//          WEB_SERVER =  'site_01.someplace.org'
-//          DB_SERVER =  'site_01.db.someplace.org'
-//          DB_NAME =    'site_01_db'
-//          SITE_URL =   'https://some-spiffy-name.org'
-//       case 'site_02':
-//          WEB_SERVER =  'site_02.someplace.org'
-//          DB_SERVER =  'site_02.db.someplace.org'
-//          DB_NAME =    'site_02_db'
-//          SITE_URL =   'https://some-different-name.org'
-//       default:
-//          error("ERROR: Unknown environment.")
-//     }
+  def setEnvironments(ENV) {
+     switch (ENV) {
+      case 'site_01':
+         WEB_SERVER =  'site_01.someplace.org'
+         DB_SERVER =  'site_01.db.someplace.org'
+         DB_NAME =    'site_01_db'
+         SITE_URL =   'https://some-spiffy-name.org'
+      case 'site_02':
+         WEB_SERVER =  'site_02.someplace.org'
+         DB_SERVER =  'site_02.db.someplace.org'
+         DB_NAME =    'site_02_db'
+         SITE_URL =   'https://some-different-name.org'
+      default:
+         error("ERROR: Unknown environment.")
+    }
 //  }
 //}
 
-def setEnvironments(ENV) {
- switch (ENV) {
-  case 'site_01':
-   def WEB_SERVER =  'site_01.someplace.org'
-   def DB_SERVER =  'site_01.db.someplace.org'
-   def DB_NAME =    'site_01_db'
-   def SITE_URL =   'https://some-spiffy-name.org'
-   break
-  case 'site_02':
-   def WEB_SERVER =  'site_02.someplace.org'
-   def DB_SERVER =  'site_02.db.someplace.org'
-   def DB_NAME =    'site_02_db'
-   def SITE_URL =   'https://some-different-name.org'
-   break
-  default:
-   error("ERROR: Unknown environment.")
-   break
- }
-}
-return this
+// def setEnvironments(ENV) {
+//  switch (ENV) {
+//   case 'site_01':
+//    def WEB_SERVER =  'site_01.someplace.org'
+//    def DB_SERVER =  'site_01.db.someplace.org'
+//    def DB_NAME =    'site_01_db'
+//    def SITE_URL =   'https://some-spiffy-name.org'
+//    break
+//   case 'site_02':
+//    def WEB_SERVER =  'site_02.someplace.org'
+//    def DB_SERVER =  'site_02.db.someplace.org'
+//    def DB_NAME =    'site_02_db'
+//    def SITE_URL =   'https://some-different-name.org'
+//    break
+//   default:
+//    error("ERROR: Unknown environment.")
+//    break
+//  }
+// }
+// return this
