6eee93bda330da60a365690c1f1a53987988f15e
Yasassri Ratnayake <ycrnet@gmail.com>
2022-08-10 20:19:02 -0400

    Create Properties.groovy

---
+++ testdir/Properties.groovy
@@ -0,0 +1,23 @@
+class Properties {
+  def WEB_SERVER
+  def DB_SERVER
+  def DB_NAME
+  def SITE_URL
+  
+  def setEnvironments(ENV) {
+     switch (ENV) {
+      case 'site_01':
+         WEB_SERVER =  'site_01.someplace.org'
+         DB_SERVER =  'site_01.db.someplace.org'
+         DB_NAME =    'site_01_db'
+         SITE_URL =   'https://some-spiffy-name.org'
+      case 'site_02':
+         WEB_SERVER =  'site_02.someplace.org'
+         DB_SERVER =  'site_02.db.someplace.org'
+         DB_NAME =    'site_02_db'
+         SITE_URL =   'https://some-different-name.org'
+      default:
+         error("ERROR: Unknown environment.")
+    }
+  }
+}
