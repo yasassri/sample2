62ed91db43ade1fcfce2872b2c277a98e977d10f
Yasassri Ratnayake <ycrnet@gmail.com>
2022-08-11 13:21:53 -0400

    Create Properties5.groovy

---
+++ testdir/Properties5.groovy
@@ -0,0 +1,18 @@
+def setEnvironments(ENV) {
+    def map[:]
+    switch (ENV) {
+      case 'site_01':
+        map["WEB_SERVER"]   =  'site_01.someplace.org'
+        map["DB_SERVER"]    =  'site_01.db.someplace.org'
+        map["DB_NAME"]      =  'site_01_db'
+        map["SITE_URL"]     =  'https://some-spiffy-name.org'
+      case 'site_02':
+        map["WEB_SERVER"]  =  'site_02.someplace.org'
+        map["DB_SERVER"]   =  'site_02.db.someplace.org'
+        map["DB_NAME"]     =  'site_02_db'
+        map["SITE_URL"]   =  'https://some-different-name.org'
+      default:
+         error("ERROR: Unknown environment.")
+    }
+    return map
+}
